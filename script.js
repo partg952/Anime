6a552152b4e8f4afd5b77fd5b9e84e76aa446116
parth <dankparth@gmail.com>
2021-03-09 20:02:33 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let num = 0
 let parent_div = document.getElementById("main-container")
 let search_container = document.getElementById("search")
+let num2=0
 
 let search = document.createElement("input")
 let button = document.createElement("input")
@@ -11,6 +12,7 @@ button.value = "Search"
 button.classList.add("button")
 search.classList.add("button")
 search.style.width = "500px"
+
 
 
 
@@ -39,6 +41,7 @@ function getAnime() {
     fetch("https://api.jikan.moe/v3/anime/" + num).then(response => {
         return response.json()
     }).then(blob => {
+        
         console.log(blob)
         p2.innerHTML = blob.title
         image.src = blob.image_url
@@ -51,8 +54,9 @@ function getAnime() {
         if (p.innerHTML == "undefined") {
             document.body.removeChild(div)
         }
-        if (time_duration.innerHTML == "null") {
-            time_duration.innerHTML = "OnGoing"
+        if (time_duration.innerHTML.includes("null")) {
+            console.log("null")
+            time_duration.innerHTML = "Total Episodes: OnGoing"
         }
     }).catch(() => {
         num = num + 1
@@ -65,8 +69,15 @@ var interval = setInterval(getAnime, 100)
 
 button.addEventListener("click", () => {
 
-    
+fetchAnimeOnClick();
+})
 
+search.addEventListener("change" , ()=>{
+    fetchAnimeOnClick()
+})
+
+function fetchAnimeOnClick(){
+    num2 = 0
     if (search.value.length != 0) {
 
         if(button.value == "Refresh"){
@@ -83,52 +94,63 @@ button.addEventListener("click", () => {
             parent_div.removeChild(parent_div.firstElementChild)
         }
 
+
         console.log("not Empty")
         clearInterval(interval)
         button.value = "Refresh"
+        
         fetch('https://api.jikan.moe/v3/search/anime?q=' + search.value + '&page=1' + search.value + "&page=1").then(response => {
             return response.json()
         }).then(blob => {
-            console.log(blob)
-            console.log(blob.results[0].image_url)
-            let ending_themes = document.createElement("p")
-            let div = document.createElement("div")
-            let p = document.createElement("p")
-            let p2 = document.createElement("p")
-            let image = document.createElement("img")
-            let themes = document.createElement("p")
-            let time_duration = document.createElement("p")
-
-            parent_div.appendChild(div)
-            div.classList.add("class")
-
-            div.appendChild(image)
-            div.appendChild(themes)
-            div.appendChild(ending_themes)
-            div.appendChild(p2)
-            div.appendChild(time_duration)
-            div.appendChild(p)
-
-            p2.classList.add("p")
-            image.src = blob.results[0].image_url
-            p2.innerHTML = blob.results[0].title
-            themes.innerHTML = "Rated: " + blob.results[0].rated
-            time_duration.innerHTML = "Total episodes:" + blob.results[0].episodes
-            p.innerHTML = blob.results[0].synopsis
-            //    time_duration.innerHTML = blob.prop.from.day + "/" + blob.prop.from.month + "/" + blob.prop.from.year + "-" + + blob.prop.to.day + "/" + blob.prop.to.month + "/" + blob.prop.to.year
-            if (p.innerHTML == "undefined") {
-                document.body.removeChild(div)
+            while(num2<50){
+                console.log(blob.results[num2].type)
+                console.log(blob)
+                console.log(blob.results[num2].image_url)
+                let ending_themes = document.createElement("p")
+                let div = document.createElement("div")
+                let p = document.createElement("p")
+                let p2 = document.createElement("p")
+                let type = document.createElement("p")
+                let image = document.createElement("img")
+                let themes = document.createElement("p")
+                let time_duration = document.createElement("p")
+    
+                parent_div.appendChild(div)
+                div.classList.add("class")
+    
+                div.appendChild(image)
+                div.appendChild(themes)
+                div.appendChild(ending_themes)
+                div.appendChild(p2)
+                div.appendChild(type)
+                div.appendChild(time_duration)
+                div.appendChild(p)
+                
+                p2.classList.add("p")
+                console.log(blob.results)
+                image.src = blob.results[num2].image_url
+                type.innerText = "Type: " + blob.results[num2].type
+                p2.innerHTML = blob.results[num2].title
+                themes.innerHTML = "Rated: " + blob.results[num2].rated
+                time_duration.innerHTML = "Total episodes:" + blob.results[num2].episodes
+                p.innerHTML = blob.results[num2].synopsis
+                //    time_duration.innerHTML = blob.prop.from.day + "/" + blob.prop.from.month + "/" + blob.prop.from.year + "-" + + blob.prop.to.day + "/" + blob.prop.to.month + "/" + blob.prop.to.year
+                if (p.innerHTML == "undefined") {
+                    document.body.removeChild(div)
+                }
+                if (time_duration.innerHTML == "null") {
+                    time_duration.innerHTML = "OnGoing"
+                }
+    
+    
+                num2++
             }
-            if (time_duration.innerHTML == "null") {
-                time_duration.innerHTML = "OnGoing"
-            }
-
-
+            
             // console.log("Searched Url Is " + blob.result.image_url)
         })
 
     } else {
         window.location.reload()
     }
-})
+}
 
